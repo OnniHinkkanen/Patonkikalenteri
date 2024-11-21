b5b6f20763952eae1d79b9d1d7a52c4bafc9d896
Onni Hinkkanen <onukilla@student.jyu.fi>
2024-11-21 16:14:02 +0200

    calendar yoinked

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 
+/*
 function App() {
   return (
     <div className="App">
@@ -19,6 +24,32 @@ function App() {
         </a>
       </header>
     </div>
+  );
+}
+**/
+
+function App() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar showDaysOutsideCurrentMonth fixedWeekNumber={6} />
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      </div>
+    </LocalizationProvider>
+
   );
 }
 
